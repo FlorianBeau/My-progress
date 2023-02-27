70cdda44d91c8389b12571efbccd88173269850a
Florian Beau <florian.beau6942@gmail.com>
2023-02-27 20:47:36 +0100

    First Save

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+import Menu from "./components/Menu";
+import TechnoAdd from "./pages/TechnoAdd";
+import "./css/app.css";
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Home />
+      <TechnoAdd />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li>Home</li>
+        <li>Add Techno</li>
+        <li>All technos</li>
+      </ul>
+    </div>
+  );
+}
