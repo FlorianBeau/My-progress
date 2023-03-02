44375605e7cb7235c844d9d54120fc891d57ee54
Florian Beau <florian.beau6942@gmail.com>
2023-03-02 17:46:20 +0100

    Save party one

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -7,13 +8,25 @@ import TechnoList from "./pages/TechnoList";
 import "./css/app.css";
 
 function App() {
+  const [technos, setTechnos] = useState([]);
+  // technos
+  // [{ name: 'React', category: 'front', description: 'Learn React'}, { name: 'Node', category: 'back', description: 'Learn Node'}]
+
+  function handleAddTechnos(techno) {
+    console.log("handleAddTechnos", techno);
+    setTechnos([...technos, techno]);
+  }
+
   return (
     <>
       <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add" element={<TechnoAdd />} />
-        <Route path="/list" element={<TechnoList />} />
+        <Route
+          path="/add"
+          element={<TechnoAdd handleAddTechno={handleAddTechnos} />}
+        />
+        <Route path="/list" element={<TechnoList technos={technos} />} />
       </Routes>
     </>
   );
--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -1,35 +1,74 @@
-export default function TechnoAdd() {
+import React, { useState } from "react";
+
+export default function TechnoAdd(props) {
+  const [techno, setTechno] = useState({
+    technoname: "",
+    technocategory: "",
+    technodescription: "",
+  });
+
+  const { handleAddTechno } = props;
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setTechno({ ...techno, [name]: value });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    handleAddTechno(techno);
+    setTechno({
+      technoname: "",
+      technocategory: "",
+      technodescription: "",
+    });
+  }
+
   return (
     <div className="techno-add">
-      <h1>Add a Techno</h1>
+      <h1>Add a techno</h1>
       <div>
-        <form>
-          <label htmlFor="techno-name">Name:</label>
+        <form onSubmit={(evt) => handleSubmit(evt)}>
+          <label htmlFor="technoname">Name:</label>
           <br />
-          <input type="text" name="techno-name" id="techno-name" />
+          <input
+            type="text"
+            name="technoname"
+            id="technoname"
+            value={techno.technoname}
+            onChange={(evt) => handleChange(evt)}
+          />
           <br />
-          <label htmlFor="techno-category">Category:</label>
+          <label htmlFor="technocategory">Category:</label>
           <br />
-          <select name="techno-category" id="techno-name">
-            <option value="">Select a category</option>{" "}
+          <select
+            name="technocategory"
+            id="technocategory"
+            value={techno.technocategory}
+            onChange={(evt) => handleChange(evt)}
+          >
+            <option value="">Select a category</option>
             <option value="front">Front</option>
             <option value="back">Back</option>
             <option value="fullstack">Full Stack</option>
             <option value="other">Other</option>
           </select>
           <br />
-          <label htmlFor="techno-description">Description:</label>
+          <label htmlFor="technodescription">Description</label>
           <br />
           <textarea
-            name="techno-description"
-            id="techno-description"
+            name="technodescription"
+            id="technodescription"
             cols="30"
             rows="10"
+            value={techno.technodescription}
+            onChange={(evt) => handleChange(evt)}
           ></textarea>
           <br />
-          <input type="submit" value="Add Techno" />
+          <input type="submit" value="Add Techno" className="btn" />
         </form>
       </div>
+      <br />
     </div>
   );
 }
