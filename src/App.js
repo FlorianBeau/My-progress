e717b498eed2c2f306c2368169d98ecf18bb6992
Florian Beau <florian.beau6942@gmail.com>
2023-03-10 21:21:52 +0100

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 import Home from "./pages/Home";
 import Menu from "./components/Menu";
 import TechnoAdd from "./pages/TechnoAdd";
 import TechnoList from "./pages/TechnoList";
 import "./css/app.css";
+import { useLocalStorage } from "./hooks/useLocalstorage";
 
 function App() {
   const [technos, setTechnos] = useState([]);
+  const STORAGE_KEY = "technos";
+  const [storedTechnos, setStoredTechnos] = useLocalStorage(STORAGE_KEY, []);
+
+  useEffect(() => {
+    console.log("UseEffect with []");
+    setTechnos(storedTechnos);
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect with [technos]");
+    setStoredTechnos(technos);
+  }, [technos]);
+
   // technos
   // [{ name: 'React', category: 'front', description: 'Learn React'}, { name: 'Node', category: 'back', description: 'Learn Node'}]
 
   function handleAddTechnos(techno) {
     console.log("handleAddTechnos", techno);
-    setTechnos([...technos, techno]);
+    setTechnos([...technos, { ...techno, technoid: uuidv4() }]);
+  }
+
+  function handleDeleteTechno(id) {
+    setTechnos(technos.filter((tech) => tech.technoid !== id));
   }
 
   return (
@@ -26,7 +45,15 @@ function App() {
           path="/add"
           element={<TechnoAdd handleAddTechno={handleAddTechnos} />}
         />
-        <Route path="/list" element={<TechnoList technos={technos} />} />
+        <Route
+          path="/list"
+          element={
+            <TechnoList
+              technos={technos}
+              handleDeleteTechno={handleDeleteTechno}
+            />
+          }
+        />
       </Routes>
     </>
   );
--- src/pages/TechnoList.js
+++ src/pages/TechnoList.js
@@ -1,17 +1,17 @@
+import TechnoItem from "../components/TechnoItem";
+
 export default function TechnoList(props) {
-  const { technos } = props;
+  const { technos, handleDeleteTechno } = props;
   return (
     <div className="techno-list">
       <h1>All Technos</h1>
       <div>
         {technos.map((techno) => (
-          <div key={techno}>
-            <h2>{techno.technoname}</h2>
-            <h3>Category</h3>
-            <p>{techno.technocategory}</p>
-            <h3>Description</h3>
-            <p>{techno.technodescription}</p>
-          </div>
+          <TechnoItem
+            techno={techno}
+            key={techno.technoid}
+            handleDeleteTechno={handleDeleteTechno}
+          />
         ))}
       </div>
     </div>
